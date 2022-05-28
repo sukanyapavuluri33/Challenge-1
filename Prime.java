36b35667a5785ad0234a04e12b902b458b59c629
sukanyapavuluri33 <101783413+sukanyapavuluri33@users.noreply.github.com>
2022-05-28 06:45:10 +0530

    Create Prime.java

---
+++ Prime.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Main {
+
+  public static void main(String[] args) {
+Scanner s=new Scanner(System.in);
+System.out.println("please enter any number");
+    int num = s.nextInt();
+    boolean flag = false;
+    for (int i = 2; i <= num / 2; ++i) {
+      // condition for nonprime number
+      if (num % i == 0) {
+        flag = true;
+        break;
+      }
+    }
+
+    if (!flag)
+      System.out.println(num + " is a prime number.");
+    else
+      System.out.println(num + " is not a prime number.");
+  }
+}
